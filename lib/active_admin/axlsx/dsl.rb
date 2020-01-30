27609936ba11c9485ea39fd8dd3ed5321c7a9a6d
Ivan Delaš <ivandelas@gmail.com>
2020-01-30 20:39:26 +0100

    Update dsl.rb

--- lib/active_admin/axlsx/dsl.rb
+++ lib/active_admin/axlsx/dsl.rb
@@ -1,12 +1,25 @@
 module ActiveAdmin
   module Axlsx
     module DSL
-      delegate :ingnore_columns, :column, :after_filer, :i18n_scope, :header_style, :skip_header, :white_list, to: :xlsx_builder, prefix: :config
-      # @see ActiveAdmin::Axlsx::Builder
-      def xlsx(options={}, &block)
-        config.xlsx_builder = ActiveAdmin::Axlsx::Builder.new(config.resource_class, options, &block)
+      delegate(:after_filter,
+               :before_filter,
+               :column,
+               :ignore_columns,
+               # TODO check + options -> :delete_columns, :only_columns, :header_format, :white_list vs. :whitelist
+               :header_style,
+               :i18n_scope,
+               :skip_header,
+               :whitelist,
+               to: :xlsx_builder,
+               prefix: :config)
+
+      def xlsx(options = {}, &block)
+        config.xlsx_builder = ActiveAdmin::Axlsx::Builder.new(
+          config.resource_class,
+          options,
+          &block
+        )
       end
     end
   end
 end
-
