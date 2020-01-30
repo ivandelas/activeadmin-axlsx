c3ed363ff053cdd936c2a3385194e4f5cf2bbd4d
Ivan Delaš <ivandelas@gmail.com>
2020-01-30 21:24:14 +0100

    Create engine.rb

---
+++ lib/active_admin/axlsx/engine.rb
@@ -0,0 +1,29 @@
+module ActiveAdmin
+  module Axlsx
+    class Engine < ::Rails::Engine
+      engine_name 'active_admin_axlsx'
+
+      initializer 'active_admin.axlsx', group: :all do
+        xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        if Mime::Type.lookup_by_extension(:xlsx).nil?
+          Mime::Type.register xlsx, :xlsx
+        end
+
+        ActiveAdmin::ResourceDSL include ActiveAdmin::Axlsx::DSL
+        ActiveAdmin::Resource include ActiveAdmin::Axlsx::ResourceExtension
+        ActiveAdmin::Views::PaginatedCollection.add_format :xlsx
+        
+        ActiveAdmin::ResourceController prepend ActiveAdmin::Axlsx::ResourceControllerExtension
+
+        # ActiveAdmin::Views::PaginatedCollection.add_format :xlsx
+
+        # ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::Axlsx::DSL
+        # ActiveAdmin::Resource.send :include, ActiveAdmin::Axlsx::ResourceExtension
+        # ActiveAdmin::ResourceController.send(
+          # :prepend,
+          # ActiveAdmin::Axlsx::ResourceControllerExtension
+        # )
+      end
+    end
+  end
+end
